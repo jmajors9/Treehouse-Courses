b07f27a09ce1ec91f5d76963374ddfda5d6918b1
joelamajors <joelamajors@gmail.com>
2018-02-15 16:35:18 -0500

    tried to start api stuff for react

---
+++ reactTrack/5-Data_Fetching_React/gif-search/src/Components/Gif.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Gif = props =>
+    <li className="gif-wrap">
+        <img alt=""/>
+    </li>
+
+export default Gif
---
+++ reactTrack/5-Data_Fetching_React/gif-search/src/Components/GifList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Gif from './Gif'
+
+const GifList = props => { 
+    return(
+        <ul className="gif-list">
+            {/* <Gif /> */}
+        </ul> 
+    );
+}
+
+export default GifList
